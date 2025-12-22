330b54785de3a38a0299a55906cd8d11d2eb67ef
BenGJ10 <bengj1015@gmail.com>
2025-12-22 12:35:15 +0530

    Setter injection

--- Spring Framework/Exploring Spring/codes/SpringDemo/src/main/java/com/bengregory/Department.java
+++ Spring Framework/Exploring Spring/codes/SpringDemo/src/main/java/com/bengregory/Department.java
@@ -2,14 +2,24 @@ package com.bengregory;
 
 public class Department {
 
+    private String name;
+
     public Department() {
         System.out.println("Department object created.");
     }
 
-    public void printDetails(){
-        System.out.println("Printing department details...");
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printDetails(){
+        System.out.println("Printing department details...");
+        System.out.println("Department: " + getName());
+    }
     
 }
 

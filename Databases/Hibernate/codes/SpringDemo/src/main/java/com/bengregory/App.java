6735a98eb9f2e9542644fcd968780181ecd9d20d
BenGJ10 <bengj1015@gmail.com>
2025-12-22 11:54:02 +0530

    Updated

---
+++ Databases/Hibernate/codes/SpringDemo/src/main/java/com/bengregory/App.java
@@ -0,0 +1,13 @@
+package com.bengregory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

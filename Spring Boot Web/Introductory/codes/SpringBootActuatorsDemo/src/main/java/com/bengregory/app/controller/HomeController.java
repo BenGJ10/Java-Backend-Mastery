0a911caf6ac2703726286862e2a88aeb7b929a4f
BenGJ10 <bengj1015@gmail.com>
2026-01-05 18:46:22 +0530

    Adding custom properties into code

--- Spring Boot Web/Introductory/codes/SpringBootActuatorsDemo/src/main/java/com/bengregory/app/controller/HomeController.java
+++ Spring Boot Web/Introductory/codes/SpringBootActuatorsDemo/src/main/java/com/bengregory/app/controller/HomeController.java
@@ -1,18 +1,29 @@
 package com.bengregory.app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Use RestController to create RESTful web services
 @RestController
 public class HomeController{
 
-    @GetMapping("/")
+    // Inject values from application properties using @Value annotation
+    @Value("${client.name}")
+    private String name;
+
+    @Value("${client.department}")
+    private String department;
+
+    // Map HTTP GET requests to the root URL ("/") and "/home"
+    @GetMapping({"/", "/home"})
     public String helloWorld(){
-        return "Hello Ben! Welcome to Spring Boot..";
+        return "Hello " + name + "! Welcome to " + department;
     }
 
+    // Map HTTP GET requests to the "/exit" URL
     @GetMapping("/exit")
     public String exitPage(){
-        return "Thank you for visiting us..";
+        return "Thank you for visiting " + department;
     }
 }
